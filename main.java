39ad6a76a77d81f882ca30f160605c98e90bfb3f
mizukiOse <m01izu01@icloud.com>
2023-05-21 17:43:12 +0900

    足し算と掛け算の計算結果

--- main.java
+++ main.java
@@ -7,7 +7,12 @@ public class main {
     int arg1=scanner.nextInt();
     int arg2=scanner.nextInt();
     carculater carculater = new carculater();
-    System.out.println(carculater.carculate(arg1, arg2));
+    System.out.println("足し算の答えは"+carculater.addition(arg1, arg2));
+
+    int arg3=scanner.nextInt();
+    int arg4=scanner.nextInt();
+    System.out.println("掛け算の答えは"+carculater.multiplication(arg3, arg4));
     scanner.close();
+
   }
 }
