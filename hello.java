340e33302a347a4ee8f1248390bbf6be3faa4051
KantaHasegawa <66783124+KantaHasegawa@users.noreply.github.com>
2023-05-21 15:41:53 +0900

    うんち

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("Hello World!!");
     System.out.println(hello());
     System.out.println("gitのお勉強");
     System.out.println("2+3");
